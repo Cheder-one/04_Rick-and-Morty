0bf6a79c6ae932a96ee24fcf02fd3e8d5a543e36
Cheder-one <cherbadenis1@gmail.com>
2023-04-07 17:11:15 +0300

    u

--- src/app/components/groupList.jsx
+++ src/app/components/groupList.jsx
@@ -3,7 +3,13 @@ import React from "react";
 const GroupList = () => {
   return (
     <div className="list-group">
-      <button type="button" className="list-group-item list-group-item-action active" aria-current="true">{"year"}</button>
+      <button
+        type="button"
+        className="list-group-item list-group-item-action active"
+        aria-current="true"
+      >
+        {"year"}
+      </button>
     </div>
   );
 };
