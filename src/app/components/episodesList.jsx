45194eff536161eae495ddbe0895dfe475599310
Cheder-one <cherbadenis1@gmail.com>
2023-04-09 22:17:34 +0300

    u

--- src/app/components/episodesList.jsx
+++ src/app/components/episodesList.jsx
@@ -1,20 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Episode from "./episode";
 import Pagination from "./pagination";
 import { getPageItems } from "../utils/paginate";
 import GroupList from "./groupList";
-import { episodes, fetchYears } from "../fakeApi/episodesApi";
+import { fetchAll, fetchYears } from "../fakeApi/episodesApi";
 
 const EpisodesList = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [episodes, setEpisodes] = useState([]);
+  const [years] = useState(fetchYears);
+  const [filter, setFilter] = useState();
+
   const totalItems = episodes.length;
   const pageSize = 8;
 
-  const [currentPage, setCurrentPage] = useState(1);
-  // const [episodes, setEpisodes] = useState([]);
-  const [years] = useState(fetchYears);
-
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
+  };
+
+  const getEpisodes = (year) => {
+    fetchAll(year).then((response) => setEpisodes(response));
   };
 
   const itemsCurntPage = getPageItems(episodes, currentPage, pageSize);
