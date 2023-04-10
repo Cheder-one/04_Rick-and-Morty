5c83afc3e3a465bf8f3fc48cac9d5ffed105cda2
Cheder-one <cherbadenis1@gmail.com>
2023-04-10 14:25:14 +0300

    u

--- src/app/components/episodesList.jsx
+++ src/app/components/episodesList.jsx
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from "react";
 import Episode from "./episode";
 import Pagination from "./pagination";
 import { getPageItems } from "../utils/paginate";
-import GroupList from "./groupList";
+import FilterList from "./filterList";
 import { fetchAll, fetchYears } from "../fakeApi/episodesApi";
 
 const EpisodesList = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [episodes, setEpisodes] = useState([]);
-  const [years] = useState(fetchYears);
+  const [years] = useState([]);
   const [filter, setFilter] = useState();
 
   const totalItems = episodes.length;
@@ -18,8 +18,18 @@ const EpisodesList = () => {
     setCurrentPage(pageNumber);
   };
 
-  const getEpisodes = (year) => {
+  // Запрашиваем список эпизодов когда...
+  const getFilteredEpisodes = (year) => {
     fetchAll(year).then((response) => setEpisodes(response));
+    setCurrentPage(1);
+  };
+  // ...меняется фильтр годов
+  useEffect(() => {
+    getFilteredEpisodes(filter);
+  }, [filter]);
+
+  const handleFilterChange = (filter) => {
+    setFilter(filter);
   };
 
   const itemsCurntPage = getPageItems(episodes, currentPage, pageSize);
@@ -28,7 +38,11 @@ const EpisodesList = () => {
       <div className="container pt-2">
         <div className="row">
           <div className="col-4">
-            <GroupList seriesYears={years} />
+            <FilterList
+              yearsEpisodes={years}
+              filter={filter}
+              onFilterChange={handleFilterChange}
+            />
           </div>
           <div className="col-8">
             <div style={{ height: "500px" }}>
